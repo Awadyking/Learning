03c094b6f30574677b2c6da91308c04b76e01b91
Ali Elawady <eali83051@gmail.com>
2024-06-01 16:52:56 +0300

    Finish html

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
+let link = "https://reqres.in";
 
+
+function login(){
+
+    let Email = document.getElementById("EIN").value
+    let Pass = document.getElementById("PIN").value
+
+axios.post(link + "/api/login" , {
+    email: Email,
+    password: Pass
+})
+
+.then((Res)=>{
+if(Res.status > 200 && Res.status < 300 ){alert("Your Token : " + Res.data.token)}
+else{alert(Res.data.error)}
+console.log(Res)
+})
+
+
+
+}
 
